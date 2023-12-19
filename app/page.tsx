fa5bc86eba9b03bc734508a5f3fe73191c10f67c
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-12-20 00:08:33 +0300

    Add Radix UI Theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
     <main>
-      <div>Hello</div>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>email</th>
+          </tr>
+        </thead>
+        <tr>
+          <td>1</td>
+          <td>John Doe</td>
+        </tr>
+      </table>
     </main>
   );
 }
