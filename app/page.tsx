9854b047e6bc340cc43aefc1b43679e55aaa5f9f
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-12-20 02:24:57 +0300

    Refactor Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,7 @@
 export default function Home() {
   return (
-    <main>
-      <table>
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>name</th>
-            <th>email</th>
-          </tr>
-        </thead>
-        <tr>
-          <td>1</td>
-          <td>John Doe</td>
-        </tr>
-      </table>
-    </main>
+   
+      <h1>Home</h1>
+   
   );
 }
