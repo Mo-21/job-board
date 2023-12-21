cefd69feab09833e8bcf04ee55a3930031aaa860
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-12-21 13:47:30 +0300

    Add Skills To Job Schema

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,9 +1,10 @@
 import { prisma } from "@/prisma/client";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Badge, Card, Flex, Heading, Text } from "@radix-ui/themes";
 import React from "react";
 import LevelBadge from "../components/LevelBadge";
 import DateFormatted from "../components/DateFormatted";
 import Link from "next/link";
+import { Job } from "@prisma/client";
 
 const page = async () => {
   const jobs = await prisma.job.findMany();
@@ -16,10 +17,23 @@ const page = async () => {
               <Heading>{job.title}</Heading>
             </Link>
             <LevelBadge level={job.level} />
+            <SkillBadge job={job} />
             <Text>{job.qualifications}</Text>
             <DateFormatted date={job.createdAt} />
           </Flex>
         </Card>
+      ))}
+    </Flex>
+  );
+};
+
+const SkillBadge = ({ job }: { job: Job }) => {
+  return (
+    <Flex gap="1" wrap="wrap">
+      {job.skills.map((skill) => (
+        <Badge variant="outline" key={skill}>
+          {skill}
+        </Badge>
       ))}
     </Flex>
   );
