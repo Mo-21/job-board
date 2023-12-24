9a12b2b6ec5eafbe0df5448883df97310d2138a1
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-12-25 01:38:29 +0300

    Add Info Icon

--- app/components/ErrorCallout.tsx
+++ app/components/ErrorCallout.tsx
@@ -1,10 +1,14 @@
 "use client";
+import { InfoCircledIcon } from "@radix-ui/react-icons";
 import { Callout } from "@radix-ui/themes";
 import React, { PropsWithChildren } from "react";
 
 const ErrorCallout = ({ children }: PropsWithChildren) => {
   return (
     <Callout.Root mb="3" color="red">
+      <Callout.Icon>
+        <InfoCircledIcon />
+      </Callout.Icon>
       <Callout.Text>{children}</Callout.Text>
     </Callout.Root>
   );
