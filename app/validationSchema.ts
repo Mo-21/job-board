d8e0449c58c9987dd5cae8f2b157f0637fdcc1d8
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-12-24 03:06:58 +0300

    Refactor validationSchema.ts to improve code readability and add password validation

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -1,32 +1,50 @@
 import { z } from "zod";
 
-export const registerSchema = z
-  .object({
-    firstName: z.string().min(1, "First Name is Required").max(255),
-    lastName: z.string().min(1, "Last Name is Required").max(255),
-    email: z.string().email("Invalid email").min(1, "Email is Required"),
-    password: z
-      .string()
-      .min(1, "Password is Required")
-      .min(8, "Password must be at least 8 characters long")
-      .refine((value) => /\d/.test(value), {
-        message: "Password must contain at least one number",
-      })
-      .refine((value) => /[a-z]/.test(value), {
-        message: "Password must contain at least one lowercase letter",
-      })
-      .refine((value) => /[A-Z]/.test(value), {
-        message: "Password must contain at least one uppercase letter",
-      })
-      .refine((value) => /[^a-zA-Z\d]/.test(value), {
-        message: "Password must contain at least one special character",
-      }),
-    passwordConfirmation: z
-      .string()
-      .min(1, "Password Confirmation is Required"),
-  })
-  .refine((data) => data.password === data.passwordConfirmation, {
-    message: "Passwords must match",
-  });
+export const registerSchema = z.object({
+  firstName: z
+    .string()
+    .min(2, "First Name is Required")
+    .max(255)
+    .regex(/^[a-zA-Z]*$/, {
+      message: "First Name must contain only English alphabets",
+    }),
+  lastName: z
+    .string({
+      invalid_type_error: "Last Name must be a string",
+    })
+    .min(2, "Last Name is Required")
+    .max(255)
+    .regex(/^[a-zA-Z]*$/, {
+      message: "Last Name must contain only English alphabets",
+    }),
+  email: z.string().email("Invalid email").min(1, "Email is Required"),
+  password: z
+    .string()
+    .min(1, "Password is Required")
+    .min(8, "Password must be at least 8 characters long")
+    .refine((value) => /\d/.test(value), {
+      message: "Password must contain at least one number",
+    })
+    .refine((value) => /[a-z]/.test(value), {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .refine((value) => /[A-Z]/.test(value), {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .refine((value) => /[^a-zA-Z\d]/.test(value), {
+      message: "Password must contain at least one special character",
+    }),
+  passwordConfirmation: z.string().min(1, "Password Confirmation is Required"),
+});
+
+export const validatePasswords = (data: RegistrationFormType) => {
+  try {
+    registerSchema.parse(data);
+    if (data.password !== data.passwordConfirmation) return false;
+    return true;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export type RegistrationFormType = z.infer<typeof registerSchema>;
