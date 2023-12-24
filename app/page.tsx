93f05a2699fb8643a8933718e4fac14d682d0254
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-12-24 03:06:43 +0300

    Add client-side code to fetch session data

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+import { useSession } from "next-auth/react";
+
 export default function Home() {
-  return (
-   
-      <h1>Home</h1>
-   
-  );
+  const { status, data: session } = useSession();
+
+  // if (status === "unauthenticated") return null;
+
+  // if(session?.user)
+  console.log(session);
+
+  return <h1>Home</h1>;
 }
