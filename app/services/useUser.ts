94f3a8b77e4558f60bc9724f255e52ce99c60090
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-12-28 01:38:00 +0300

    Create Custom Hook To Fetch Users With React Query

--- app/services/useUser.ts
+++ app/services/useUser.ts
@@ -1,9 +1,10 @@
+import { User } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 export const useUser = (userId: string) =>
-  useQuery({
+  useQuery<User>({
     queryKey: ["users"],
-    queryFn: () => axios.get(`/api/users/${userId}`),
+    queryFn: () => axios.get(`/api/users/${userId}`).then((res) => res.data),
     staleTime: 1000 * 60 * 30,
   });
