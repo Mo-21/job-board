723bde4660e66d8b1b76b9a15deb236470d5f3b7
Mohammad Malaeb <m.malaeb21@gmail.com>
2023-12-30 01:33:55 +0300

    Create Protected Route To Be Accessed Only By Recruiters

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,15 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  callbacks: {
+    authorized: async ({ req, token }) => {
+      if (req.nextUrl.pathname.startsWith("/recruiters"))
+        return token?.role === "RECRUITER";
+      return !!token;
+    },
+  },
+});
 
 export const config = {
-  matcher: ["/register/profile/:id+"],
+  matcher: ["/users/profile/edit/:id+", "/recruiters"],
 };
