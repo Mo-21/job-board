c5ea17712d4913485136d56020df8882746604fe
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-01-04 02:32:39 +0300

    Fix Typography With Tailwind

--- app/jobs/[id]/_components/JobDetails.tsx
+++ app/jobs/[id]/_components/JobDetails.tsx
@@ -2,23 +2,32 @@ import DateFormatted from "@/app/components/DateFormatted";
 import LevelBadge from "@/app/components/LevelBadge";
 import { SkillBadge } from "@/app/components/SkillBadge";
 import { Job } from "@prisma/client";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Card, Flex, Heading, Text } from "@radix-ui/themes";
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
 const JobDetails = ({ job }: { job: Job }) => {
   return (
     <Card className="md:col-span-3">
-      <Flex p="2" gap="2" align="center" direction="column">
+      <Flex p="2" gap="2" direction="column">
         <Heading>{job.title}</Heading>
-        <LevelBadge level={job.level} />
+        <Flex>
+          <LevelBadge level={job.level} />
+        </Flex>
         <SkillBadge job={job} />
-        <Text>
-          {job.company} - {job.location}
-        </Text>
-        <DateFormatted date={job.createdAt} />
-        <ReactMarkdown>{job.description}</ReactMarkdown>
-        <ReactMarkdown>{job.qualifications}</ReactMarkdown>
+        <Flex justify="between">
+          <Text className="font-medium">
+            {job.company} - {job.location}
+          </Text>
+          <DateFormatted date={job.createdAt} />
+        </Flex>
+        <Box className="prose max-w-full mt-3">
+          <Heading size="4">Description:</Heading>
+          <ReactMarkdown>{job.description}</ReactMarkdown>
+
+          <Heading size="4">Qualifications:</Heading>
+          <ReactMarkdown>{job.qualifications}</ReactMarkdown>
+        </Box>
       </Flex>
     </Card>
   );
