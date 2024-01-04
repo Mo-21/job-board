a63524991a348f45347e8f47fe28495d9dc137cd
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-01-04 19:46:25 +0300

    Custom Hook To Post UserId To The Applied Job

---
+++ app/jobs/[id]/services/applyForJob.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { useState } from "react";
+
+const useApply = (JobId: string | undefined) => {
+  const [status, setStatus] = useState<number | null>(null);
+
+  const applyForJob = async (data: { userId: string; resumeId: string }) => {
+    try {
+      const res = await axios.patch("/", data);
+      setStatus(res.status);
+    } catch (error) {
+      console.error("Error applying for job:", error);
+    }
+  };
+
+  return { applyForJob, status };
+};
+
+export default useApply;
