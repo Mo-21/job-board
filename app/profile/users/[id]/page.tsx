aa4639c9bc500388b37079e8929881cda538b9d1
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-01-05 02:23:42 +0300

    Refactor User Profile And Add Jobs Applied

--- app/profile/users/[id]/page.tsx
+++ app/profile/users/[id]/page.tsx
@@ -1,4 +1,6 @@
+import authOptions from "@/app/auth/authOptions";
 import UserImage from "@/app/components/UserImage";
+import JobList from "@/app/jobs/_components/JobList";
 import { prisma } from "@/prisma/client";
 import { Education, Project, User, Work_Experience } from "@prisma/client";
 import {
@@ -11,10 +13,13 @@ import {
   Text,
   Button,
 } from "@radix-ui/themes";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 import React from "react";
 
 const ProfilePage = async ({ params }: { params: { id: string } }) => {
+  const session = await getServerSession(authOptions);
+
   const user = await prisma.user.findUnique({
     where: {
       id: params.id,
@@ -40,18 +45,19 @@ const ProfilePage = async ({ params }: { params: { id: string } }) => {
 
   if (!user) return <Text>Not Found</Text>;
 
+  const jobsApplied = await prisma.job.findMany({
+    where: { usersId: { has: user.id } },
+  });
+
   const tables = [
     { label: "Personal Info", value: <PersonalInfo user={user} /> },
-    { label: "Skills", value: <Skills user={user} /> },
+    { label: "Skills", value: <Skills skills={user.skills} /> },
     { label: "Projects", value: <Projects projects={user.projects} /> },
     {
       label: "Education",
       value: <EducationHistory education={user.education} />,
     },
-    {
-      label: "Work Experience",
-      value: <Work work={user.workExperience} />,
-    },
+    { label: "Work Experience", value: <Work work={user.workExperience} /> },
   ];
 
   return (
@@ -72,17 +78,20 @@ const ProfilePage = async ({ params }: { params: { id: string } }) => {
             }}
           />
           <Text>{user.bio}</Text>
-          <Button variant="solid">
-            <Link href={`/profile/edit/${user.id}`}>Edit Profile</Link>
-          </Button>
+          {session?.user.id === user.id && (
+            <Button variant="solid">
+              <Link href={`/profile/edit/${user.id}`}>Edit Profile</Link>
+            </Button>
+          )}
         </Flex>
         <Flex direction="column" gap="7" className="col-span-4">
           {tables.map((table) => (
             <Flex direction="column" gap="3" key={table.label}>
-              <Heading size="5">{table.label}</Heading>
               {table.value}
             </Flex>
           ))}
+          <Heading>Jobs Applied</Heading>
+          <JobList jobs={jobsApplied} />
         </Flex>
       </Grid>
     </Container>
@@ -91,160 +100,181 @@ const ProfilePage = async ({ params }: { params: { id: string } }) => {
 
 const PersonalInfo = ({ user }: { user: User }) => {
   return (
-    <Table.Root className="col-span-3" color="green">
-      <Table.Header>
-        <Table.Row>
-          <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>Location</Table.ColumnHeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        <Table.Row>
-          <Table.Cell>
-            <Text>{user.name}</Text>
-          </Table.Cell>
-          <Table.Cell>
-            <Text>{user.email}</Text>
-          </Table.Cell>
-          <Table.Cell>
-            <Text>{user.location}</Text>
-          </Table.Cell>
-        </Table.Row>
-      </Table.Body>
-    </Table.Root>
+    <>
+      <Heading size="5">Personal Info</Heading>
+      <Table.Root className="col-span-3" color="green">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Location</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>
+              <Text>{user.name}</Text>
+            </Table.Cell>
+            <Table.Cell>
+              <Text>{user.email}</Text>
+            </Table.Cell>
+            <Table.Cell>
+              <Text>{user.location}</Text>
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table.Root>
+    </>
   );
 };
 
-const Skills = ({ user }: { user: User }) => {
+const Skills = ({ skills }: { skills: string[] }) => {
+  if (skills.length === 0) return null;
   return (
-    <Table.Root className="col-span-3" color="green">
-      <Table.Header>
-        <Table.Row>
-          <Table.ColumnHeaderCell>Skills</Table.ColumnHeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        <Table.Row>
-          <Table.Cell>
-            {user.skills.map((skill) => (
-              <Badge mr="2" key={skill}>
-                {skill}
-              </Badge>
-            ))}
-          </Table.Cell>
-        </Table.Row>
-      </Table.Body>
-    </Table.Root>
-  );
-};
-const Projects = ({ projects }: { projects: Project[] }) => {
-  return (
-    <Table.Root className="col-span-3" color="green">
-      <Table.Header>
-        <Table.Row>
-          <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>Description</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>Skills</Table.ColumnHeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {projects.map((project) => (
-          <Table.Row key={project.id}>
+    <>
+      <Heading size="5">Skills</Heading>
+      <Table.Root className="col-span-3" color="green">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Skills</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row>
             <Table.Cell>
-              <Text>{project.title}</Text>
-            </Table.Cell>
-            <Table.Cell>
-              <Text>{project.description}</Text>
-            </Table.Cell>
-            <Table.Cell>
-              {project.skills.map((skill) => (
+              {skills.map((skill) => (
                 <Badge mr="2" key={skill}>
                   {skill}
                 </Badge>
               ))}
             </Table.Cell>
           </Table.Row>
-        ))}
-      </Table.Body>
-    </Table.Root>
+        </Table.Body>
+      </Table.Root>
+    </>
   );
 };
-const EducationHistory = ({ education }: { education: Education[] }) => {
+
+const Projects = ({ projects }: { projects: Project[] }) => {
+  if (projects.length === 0) return null;
   return (
-    <Table.Root className="col-span-3" color="green">
-      <Table.Header>
-        <Table.Row>
-          <Table.ColumnHeaderCell>Degree</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>School</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>Location</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>Start Date</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>End Date</Table.ColumnHeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {education.map((edu) => (
-          <Table.Row key={edu.id}>
-            <Table.Cell>
-              <Text>{edu.degree}</Text>
-            </Table.Cell>
-            <Table.Cell>
-              <Text>{edu.school}</Text>
-            </Table.Cell>
-            <Table.Cell>
-              <Text>{edu.location}</Text>
-            </Table.Cell>
-            <Table.Cell>
-              <Text>{edu.startDate.toISOString().split("T")[0]}</Text>
-            </Table.Cell>
-            <Table.Cell>
-              <Text>{edu.endDate.toISOString().split("T")[0]}</Text>
-            </Table.Cell>
+    <>
+      <Heading size="5">Projects</Heading>
+      <Table.Root className="col-span-3" color="green">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Title</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Description</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Skills</Table.ColumnHeaderCell>
           </Table.Row>
-        ))}
-      </Table.Body>
-    </Table.Root>
+        </Table.Header>
+        <Table.Body>
+          {projects.map((project) => (
+            <Table.Row key={project.id}>
+              <Table.Cell>
+                <Text>{project.title}</Text>
+              </Table.Cell>
+              <Table.Cell>
+                <Text>{project.description}</Text>
+              </Table.Cell>
+              <Table.Cell>
+                {project.skills.map((skill) => (
+                  <Badge mr="2" key={skill}>
+                    {skill}
+                  </Badge>
+                ))}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </>
+  );
+};
+
+const EducationHistory = ({ education }: { education: Education[] }) => {
+  if (education.length === 0) return null;
+  return (
+    <>
+      <Heading size="5">Education</Heading>
+      <Table.Root className="col-span-3" color="green">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Degree</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>School</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Location</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Start Date</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>End Date</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {education.map((edu) => (
+            <Table.Row key={edu.id}>
+              <Table.Cell>
+                <Text>{edu.degree}</Text>
+              </Table.Cell>
+              <Table.Cell>
+                <Text>{edu.school}</Text>
+              </Table.Cell>
+              <Table.Cell>
+                <Text>{edu.location}</Text>
+              </Table.Cell>
+              <Table.Cell>
+                <Text>{edu.startDate.toISOString().split("T")[0]}</Text>
+              </Table.Cell>
+              <Table.Cell>
+                <Text>{edu.endDate.toISOString().split("T")[0]}</Text>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </>
   );
 };
 
 const Work = ({ work }: { work: Work_Experience[] }) => {
+  if (work.length === 0) return null;
   return (
-    <Table.Root className="col-span-3" color="green">
-      <Table.Header>
-        <Table.Row>
-          <Table.ColumnHeaderCell>Degree</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>School</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>Location</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>Description</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>Start Date</Table.ColumnHeaderCell>
-          <Table.ColumnHeaderCell>End Date</Table.ColumnHeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {work.map((work) => (
-          <Table.Row key={work.id}>
-            <Table.Cell>
-              <Text>{work.title}</Text>
-            </Table.Cell>
-            <Table.Cell>
-              <Text>{work.company}</Text>
-            </Table.Cell>
-            <Table.Cell>
-              <Text>{work.location}</Text>
-            </Table.Cell>
-            <Table.Cell>
-              <Text>{work.description}</Text>
-            </Table.Cell>
-            <Table.Cell>
-              <Text>{work.startDate.toISOString().split("T")[0]}</Text>
-            </Table.Cell>
-            <Table.Cell>
-              <Text>{work.endDate.toISOString().split("T")[0]}</Text>
-            </Table.Cell>
+    <>
+      <Heading size="5">Work Experience</Heading>
+      <Table.Root className="col-span-3" color="green">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Degree</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>School</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Location</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Description</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Start Date</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>End Date</Table.ColumnHeaderCell>
           </Table.Row>
-        ))}
-      </Table.Body>
-    </Table.Root>
+        </Table.Header>
+        <Table.Body>
+          {work.map((work) => (
+            <Table.Row key={work.id}>
+              <Table.Cell>
+                <Text>{work.title}</Text>
+              </Table.Cell>
+              <Table.Cell>
+                <Text>{work.company}</Text>
+              </Table.Cell>
+              <Table.Cell>
+                <Text>{work.location}</Text>
+              </Table.Cell>
+              <Table.Cell>
+                <Text>{work.description}</Text>
+              </Table.Cell>
+              <Table.Cell>
+                <Text>{work.startDate.toISOString().split("T")[0]}</Text>
+              </Table.Cell>
+              <Table.Cell>
+                <Text>{work.endDate.toISOString().split("T")[0]}</Text>
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </>
   );
 };
 
